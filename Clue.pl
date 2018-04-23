5300e3cb37e45cf4306eb9ea061c2391de16dce9
Ovi Fritsch <ovidiu.fritsch@gmail.com>
2018-04-23 15:29:24 -0700

    minimal requirements pretty much done. lightly tested accusation and seems to work fine. added a README. check it out.

--- Clue.pl
+++ Clue.pl
@@ -20,7 +20,7 @@ shown(Player,H) :- player_num(Y), set(Player,H,Y).
 
 not_any(Player,Person,Weapon,Room) :- player_num(X), Player < X, valid_person(Person), valid_weapon(Weapon), valid_room(Room), set_not(Player,Person,Weapon,Room).
 
-%
+% not sure if well need any of this
 suggestion(Player,Person,Weapon,Room) :- player_num(X), Player < X, me(Y), Player = me, valid_person(Person), valid_weapon(Weapon), valid_room(Room).
 
 %
@@ -28,8 +28,14 @@ suggestion(Player,Person,Weapon,Room) :- player_num(X), Player < X, me(Y), Playe
 
 
 %This returns true if for every category, nobody has X AND for every card other than X, somebody has it.
-make_accusation(Person, Weapon, Room) :- know(Person), know(Weapon), know(Room). 
+make_accusation(Person, Weapon, Room) :- know(Person), know(Weapon), know(Room).
 
+% Person, Weapon, and Room are the suggested objects and Responses is a List of at most 3 elements, each of which is a 2 element list containing the
+%player number who showed me the card and the card that they showed me.
+% The predicate adds the information given to the knowledgebase.
+%later stages should do something with Person, Weapon, and Room
+
+my_suggestion(Person, Weapon, Room, Responses) :- add_responses(Responses).
 
 %Supporting code: Users dont need to look here
 
@@ -50,14 +56,18 @@ setup_have(1,H) :- assert(could_have(1,H)).
 setup_have(X,H) :- assert(could_have(X,H)), succ(X0, X), setup_have(X0,H).
 
 %supporting predicates for hand
+
+attach_people([], Player).
 attach_people([H],Player) :- valid_person(H), shown(Player,H).
-attach_people([H|T],Player) :- valid_person(H), shown(Player,H), attach_people(T).
+attach_people([H|T],Player) :- valid_person(H), shown(Player,H), attach_people(T, Player).
 
+attach_weapons([], Player).
 attach_weapons([H],Player) :- valid_weapon(H), shown(Player,H).
-attach_weapons([H|T],Player) :- valid_weapon(H), shown(Player,H), attach_weapons(T).
+attach_weapons([H|T],Player) :- valid_weapon(H), shown(Player,H), attach_weapons(T, Player).
 
+attach_rooms([], Player).
 attach_rooms([H],Player) :- valid_room(H), shown(Player,H).
-attach_rooms([H|T],Player) :- valid_room(H), shown(Player,H), attach_rooms(T).
+attach_rooms([H|T],Player) :- valid_room(H), shown(Player,H), attach_rooms(T, Player).
 
 %establishes who has what, and then removes could_have from all players, sets has for the passed player and could
 
@@ -71,22 +81,23 @@ set_not(Player,Person,Weapon,Room) :- retract(could_have(Player,Person)), assert
 
 %Supporting code for make_accusation
 
-know(Person) :- player_num(NumPlayers), people(PeopleList), valid_person(Person), not(somebody_has(NumPlayers, Person)), last_standing(Person, PeopleList).
+know(Person) :- player_num(NumPlayers), people(PeopleList), valid_person(Person), not(somebody_has(NumPlayers, Person)), last_standing(Person, PeopleList), !.
 know(Weapon) :- player_num(NumPlayers), weapons(WeaponsList), valid_weapon(Weapon), not(somebody_has(NumPlayers, Weapon)), last_standing(Weapon, WeaponsList).
 know(Room) :- 	player_num(NumPlayers), rooms(RoomsList), valid_room(Room), not(somebody_has(NumPlayers, Room)), last_standing(Room, RoomsList).
 
 somebody_has(1, Card) :- has(1, Card).
 somebody_has(Player, Card) :- has(Player, Card).
-somebody_has(Player, Card) :- succ(PrevPlayer, Player), somebody_has_person(PrevPlayer, Card).
+somebody_has(Player, Card) :- succ(PrevPlayer, Player), somebody_has(PrevPlayer, Card).
 
 
 % true if all People except for Person is known to be held by some Player.
 last_standing(Card, [Cards_Head]) :- Card == Cards_Head.
 last_standing(Card, [Cards_Head]) :- player_num(NumPlayers), somebody_has(NumPlayers, Cards_Head).
-last_standing(Card, [Cards_Head | Cards_Tail]) :- Card == Cards_Head, last_standing(Person, Cards_Tail).
-last_standing(Card, [Cards_Head | Cards_Tail]) :- Card \== Cards_Head, player_num(NumPlayers), somebody_has(NumPlayers, Cards_Head), last_standing(Person, Cards_Tail).
+last_standing(Card, [Cards_Head | Cards_Tail]) :- Card == Cards_Head, last_standing(Card, Cards_Tail).
+last_standing(Card, [Cards_Head | Cards_Tail]) :- Card \== Cards_Head, player_num(NumPlayers), somebody_has(NumPlayers, Cards_Head), last_standing(Card, Cards_Tail).
 
 
-
-
+%my_suggestion helpers
+add_responses([[Player | Card]]) :- shown(Player, Card).
+add_responses([[Player | Card] | Tail]) :- shown(Player, Card), add_responses(Tail).
 
